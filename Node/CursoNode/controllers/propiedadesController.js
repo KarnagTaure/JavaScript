28fc01613837b97c00d60baf00fdc506cfa5a8a2
KRNGDev <161486725+KRNGDev@users.noreply.github.com>
2024-04-04 13:55:42 +0200

    up

--- Node/CursoNode/controllers/propiedadesController.js
+++ Node/CursoNode/controllers/propiedadesController.js
@@ -108,7 +108,7 @@ const agregarImagen = async (req, res) => {
 
 
   res.render("propiedades/agregar-imagen",{
-    pagina: "Agregar Imagen",
+    pagina: "Agregar Imagen: ${propiedad.titulo}",
     propiedad
   });
 };
